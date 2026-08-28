a1465f14bf0ae85426bb75593eae849a1b2ef916
bert hubert <bert@hubertnet.nl>
2026-08-28 12:22:02 +0200

    should make it work on modern libgpiod

--- solcount.cc
+++ solcount.cc
@@ -1,10 +1,14 @@
 #include <gpiod.hpp>
-#include "ext/powerblog/h2o-pp.hh"
 #include <thread>
 #include <atomic>
 #include <iostream>
 #include <chrono>
 #include <signal.h>
+#define CPPHTTPLIB_USE_POLL
+#define CPPHTTPLIB_THREAD_POOL_COUNT 32
+
+#include "httplib.h"
+
 
 using Clock = std::chrono::steady_clock; 
 
@@ -24,7 +28,7 @@ static double passedMsec(const Clock::time_point& then)
 using namespace std;		// No need to keep using
 
 std::atomic<uint64_t> g_pulses;
-
+#if 0
 void print_event(const ::gpiod::line_event& event)
 {
 	
@@ -45,70 +49,82 @@ void print_event(const ::gpiod::line_event& event)
 
 	::std::cout << ::std::endl;
 }
-
+#endif
 
 int main(int argc, char **argv)
 {
- 	signal(SIGPIPE, SIG_IGN); // every TCP application needs this
+  signal(SIGPIPE, SIG_IGN); // every TCP application needs this
 
-	if (argc < 3) {
-		cout << "usage: " << argv[0] << " <chip> <offset0> ..." << endl;
-		return EXIT_FAILURE;
-	}
+  if (argc < 3) {
+    cout << "usage: " << argv[0] << " <chip> <offset0> ..." << endl;
+    return EXIT_FAILURE;
+  }
 
-	vector<unsigned int> offsets;
-	offsets.reserve(argc);
-	for (int i = 2; i < argc; i++)
-		offsets.push_back(stoul(argv[i]));
+  vector<unsigned int> offsets;
+  offsets.reserve(argc);
+  for (int i = 2; i < argc; i++)
+    offsets.push_back(stoul(argv[i]));
 
-	::gpiod::chip chip(argv[1]);
-	auto lines = chip.get_lines(offsets);
 
-	lines.request({
-		argv[0],
-		::gpiod::line_request::EVENT_FALLING_EDGE,
-		0,
-	});
+  auto request =
+    ::gpiod::chip(argv[1])
+    .prepare_request()
+    .set_consumer("watch-line-value")
+    .add_line_settings(
+		       atoi(argv[2]),
+		       ::gpiod::line_settings()
+		       .set_direction(
+				      ::gpiod::line::direction::INPUT)
+		       .set_edge_detection(
+					   ::gpiod::line::edge::FALLING)
+		       )
+    .do_request();
 
-	H2OWebserver h2s("solcount");
 	
-	/*
-	# HELP http_requests_total The total number of HTTP requests.
-	# TYPE http_requests_total counter
-	http_requests_total{method="post",code="200"} 1027 1395066363000
-	http_requests_total{method="post",code="400"}    3 1395066363000
-	*/
 	
-	h2s.addHandler("/metrics",[](auto handler, auto req) 
-        {
-        	ostringstream ret;
-        	ret << "# HELP power_pulses The total number of power pulses"<<endl;	
-        	ret << "# TYPE power_pulses counter"<<endl;
-        	ret<<"power_pulses "<<g_pulses<<endl;
-                   return pair<string,string>("text/plain", ret.str());
-                 });
+  /*
+    # HELP http_requests_total The total number of HTTP requests.
+    # TYPE http_requests_total counter
+    http_requests_total{method="post",code="200"} 1027 1395066363000
+    http_requests_total{method="post",code="400"}    3 1395066363000
+  */
 
+  httplib::Server svr;
+  svr.Get(R"(/metrics)", [](const auto &req, auto&res) {
+    ostringstream ret;
+    ret << "# HELP power_pulses The total number of power pulses"<<endl;	
+    ret << "# TYPE power_pulses counter"<<endl;
+    ret<<"power_pulses "<<g_pulses<<endl;
+    res.set_content(ret.str(), "text/plain");	  
+  });
+	
+  std::thread ws([&svr]() {
+    svr.set_socket_options([](socket_t sock) {
+      int yes = 1;
+      setsockopt(sock, SOL_SOCKET, SO_REUSEADDR,
+		 reinterpret_cast<const void *>(&yes), sizeof(yes));
+    });
+	  
+    cout<<"Going live on http://0.0.0.0:10000/\n";
+    svr.listen("0.0.0.0", 10000);
+  });
+  ws.detach();
 
-	std::thread ws([&h2s]() {
-	      auto actx = h2s.addContext();
-	      ComboAddress listenOn("0.0.0.0:10000");
-	      h2s.addListener(listenOn, actx);
-	      cout<<"Listening on "<< listenOn.toStringWithPort() <<endl;
-	      h2s.runLoop();
-	    });
-	ws.detach();
+  //  auto prev = Clock::now();
 
+  ::gpiod::edge_event_buffer buffer(1);
 
-	for (;;) {
-		auto events = lines.event_wait(chrono::seconds(1));
-		if (events) {
-			for (auto& it: events) {
-				it.event_read();
-				g_pulses++;
-				cout<<g_pulses<<endl;
-//				print_event(it.event_read());
-			}
-		}
-	}
-
+  for (;;) {
+    request.read_edge_events(buffer);
+    
+    for (const auto &event : buffer) {
+      ::std::cout << "line: " << event.line_offset()
+		  << "  event #" << event.line_seqno()
+		  << ::std::endl;
+      
+      g_pulses++;
+    }
+  }
 }
+
+
